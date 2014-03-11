cc62ba263a2f6978ccab7c1d06004394ff502eaf
avest <avest1000@gmail.com>
2014-03-12 00:20:14 +0800

    之前把运算任务分成多个程序运行，而本工程实现多个子结果的合并。

--- CalDup/CalDup.cpp
+++ CalDup/CalDup.cpp
@@ -7,19 +7,19 @@
 using namespace std;
 
 #define LineNum 551
+#define DocNum 10
 #define ExpeNum 4
-#define DocNum 5
 
-long long m[DocNum][ExpeNum][LineNum][3];
+
+long long m[DocNum][ExpeNum][LineNum][2];
 int dupCount = 2701376;
 
 /*
-共5个文件doc0-4
-doc1=doc0-doc1
-doc1每行记录累加
-再把doc1到doc4累加
+共DocNum个文件
+把记录累加
 最后输出
 */
+
 
 int main()
 {
@@ -31,7 +31,7 @@ int main()
         {
             for(int k = 0; k < LineNum; k++)
             {
-                for(int x = 0; x < 3; x++)
+                for(int x = 0; x < 2; x++)
                 {
                     scanf("%lld", &m[i][j][k][x]);
                 }
@@ -42,26 +42,13 @@ int main()
     {
         for(int k = 0; k < LineNum; k++)
         {
-            m[1][j][k][1] = m[0][j][k][1] - m[1][j][k][1];
-            m[1][j][k][2] = m[0][j][k][2] - m[1][j][k][2];
-            if(k > 0)
-            {
-                m[1][j][k][1] += m[1][j][k-1][1];
-                m[1][j][k][2] += m[1][j][k-1][2];
-            }
-        }
-    }
-    for(int j = 0; j < ExpeNum; j++)
-    {
-        for(int k = 0; k < LineNum; k++)
-        {
             long long hitNum = 0, dupNum = 0;
-            for(int i = 1; i < DocNum; i++)
+            for(int i = 0; i < DocNum; i++)
             {
-                hitNum += m[i][j][k][1];
-                dupNum += m[i][j][k][2];
+                hitNum += m[i][j][k][0];
+                dupNum += m[i][j][k][1];
             }
-            printf("for %lld cmps, find %lld dups, recall is %lf\n", hitNum, dupNum, (double)dupNum/dupCount);
+            printf("for %lld cmps, find %lld dups, recall is %.8lf\n", hitNum, dupNum, (double)dupNum/dupCount);
             if(dupNum == dupCount)
                 break;
         }
